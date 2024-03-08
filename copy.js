9fe437ce57c97019e4f4d9a8c81ef6654dd99506
Emadstrong <132583034+Emadstrong@users.noreply.github.com>
2024-03-08 11:35:49 +0300

    Update copy.js

--- copy.js
+++ copy.js
@@ -130,7 +130,7 @@ function main() {
     isBackup = args[0] === 'backup';
     isClone = args[0] === 'clone';
     isRestore = args[0] === 'restore';
-    client.login(process.env.TOKEN);
+    await client.login(process.env.BOT_TOKEN);
 }
 
 main();
